0b1ab995d094a783751ee1bf830380f1683ef9a5
KiriN <if23b229@technikum-wien.at>
2024-03-31 11:35:03 +0200

    "Grundbasis der Webseite"

---
+++ header.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en"><!--Unsere Webseite Sprache wird englsich sein***-->
+<?php session_start();?>
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <title>Library</title>
+        <!-- Favicon-->
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+        <!-- Bootstrap Icons-->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" rel="stylesheet" />
+        <!-- Google fonts-->
+        <link href="https://fonts.googleapis.com/css?family=Merriweather+Sans:400,700" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic" rel="stylesheet" type="text/css" />
+        <!-- SimpleLightbox plugin CSS-->
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/SimpleLightbox/2.1.0/simpleLightbox.min.css" rel="stylesheet" />
+        
+        <!-- Core theme CSS (includes Bootstrap)-->
+        <link href="../ITP_Group8_BOOKWARM/css/style.css" rel="stylesheet" />
+    </head>
+    <body id="page-top">
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-light fixed-top py-3" id="mainNav">
+            <div class="container px-4 px-lg-5">
+                <a class="navbar-brand" href="index.php#page-top">Library</a>
+                <button class="navbar-toggler navbar-toggler-right" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ms-auto my-2 my-lg-0">
+                        <li class="nav-item"><a class="nav-link" href="index.php#about">About</a></li>
+                        <li class="nav-item"><a class="nav-link" href="index.php#services">Services</a></li>
+                        <li class="nav-item"><a class="nav-link" href="viewBook.php">Book Gallery</a></li>
+                        <?php
+                            if(isset($_SESSION["loged"])){
+                                echo '<li class="nav-item"><a class="nav-link" href="addBook.php">Add Books</a></li>';
+                                echo '<li class="nav-item"><a class="nav-link" href="#">'.$_SESSION['firstname'].' '.$_SESSION['lastname'].'</a></li>';                                
+                                echo '<li class="nav-item"><a class="nav-link" href="logout.php">Logout</a></li>';
+                            }
+                            else{
+                                echo '<li class="nav-item"><a class="nav-link" href="login.php">Login</a></li>';
+                            }
+                        ?>
+                    </ul>
+                </div>
+            </div>
+        </nav>
