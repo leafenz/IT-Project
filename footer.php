18a03fbb6170a71a869808b930f25608b6d18f2b
KiriN <if23b229@technikum-wien.at>
2024-03-31 11:35:03 +0200

    "Grundbasis der Webseite"

---
+++ footer.php
@@ -0,0 +1,62 @@
+<footer class="page-footer font-small" style="background-color:white; margin: 10px">
+
+  <div style="background-color: rgb(255,10,69)">
+    <div class="container">
+
+      <div class="row py-4 d-flex align-items-center">
+
+
+      </div>
+
+    </div>
+  </div>
+
+  <div class="container text-center text-md-left mt-5" >
+
+    <div class="row mt-3">
+
+      <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+
+        <h6 class="text-uppercase font-weight-bold">Online Library</h6>
+        <hr class="deep-purple accent-2 mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;">
+        <p>BOOKWARMWORM</p>
+
+      </div>
+
+      <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+
+        <h6 class="text-uppercase font-weight-bold">Sites</h6>
+        <hr class="deep-purple accent-2 mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;">
+        <p>
+        <a href="index.php#about">About</a>
+        </p>
+        <p>
+          <a href="index.php#services">Services</a>
+        </p>
+
+      </div>
+      
+      <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+
+        <h6 class="text-uppercase font-weight-bold">Sites</h6>
+        <hr class="deep-purple accent-2 mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;">
+        <p>
+          <a href="viewBook.php">Book Gallery</a>
+        </p>
+        <p>
+          <a href="login.php">Login</a>
+        </p>
+
+      </div>
+
+      
+    </div>
+
+  </div>
+  <div class="bg-light py-5">
+    <div class="container px-4 px-lg-5"><div class="small text-center text-muted">Copyright &copy; 2024</div></div>
+        </div>
+
+
+</footer>
+</html>
---
+++ index.php
@@ -0,0 +1,65 @@
+<?php
+    include "header.php";
+?>
+        <!-- Masthead-->
+        <header class="masthead">
+            <div class="container px-4 px-lg-5 h-100">
+                <div class="row gx-4 gx-lg-5 h-100 align-items-center justify-content-center text-center">
+                    <div class="col-lg-8 align-self-end">
+                        <h1 class="text-white font-weight-bold">Library</h1>
+                        <hr class="divider" />
+                    </div>
+                    <div class="col-lg-8 align-self-baseline">
+                    </div>
+                </div>
+            </div>
+        </header>
+        <!-- About-->
+        <section class="page-section bg-primary" id="about">
+            <div class="container px-4 px-lg-5">
+                <div class="row gx-4 gx-lg-5 justify-content-center">
+                    <div class="col-lg-8 text-center">
+                        <h2 class="text-white mt-0">We got what you need!</h2>
+                        <hr class="divider divider-light" />
+                        <p class="text-white-75 mb-4">This website is an online Library! Here you can see books form other clients that have uploaded. You can upload after you have registered.</p>
+                        <a class="btn btn-light btn-xl" href="register.php">Get Started!</a>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- Services-->
+        <section class="page-section" id="services">
+            <div class="container px-4 px-lg-5">
+                <h2 class="text-center mt-0">Our Services</h2>
+                <hr class="divider" />
+                <div class="row gx-4 gx-lg-5">
+                    <div class="col-lg-3 col-md-6 text-center">
+                        <div class="mt-5">
+                            <div class="mb-2"><i class="bi bi-person fs-1 text-primary"></i></div>
+                            <h3 class="h4 mb-2">Login</h3>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-6 text-center">
+                        <div class="mt-5">
+                            <div class="mb-2"><i class="bi bi-person-plus fs-1 text-primary"></i></div>
+                            <h3 class="h4 mb-2">Register</h3>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-6 text-center">
+                        <div class="mt-5">
+                            <div class="mb-2"><i class="bi-plus fs-1 text-primary"></i></div>
+                            <h3 class="h4 mb-2">Add Books</h3>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-6 text-center">
+                        <div class="mt-5">
+                            <div class="mb-2"><i class="bi bi-book fs-1 text-primary"></i></div>
+                            <h3 class="h4 mb-2">View Books</h3>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+<?php
+    include "footer.php";
+?>
