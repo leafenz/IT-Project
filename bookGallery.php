11e2d3de27d63905386b2decb5dc4a8347d8d22c
Wiktoria Niziolek <wiktoria.niziolek@gmail.com>
2024-05-23 14:47:37 +0200

    small pfad change

--- bookGallery.php
+++ bookGallery.php
@@ -2,12 +2,6 @@
 include 'db_connection.php';
 include 'header.php';
 
-if (!isset($_SESSION['username'])) {
-    // If the user is not logged in, redirect to the login page
-    header("Location: login.php");
-    exit();
-}
-
 $conn = mysqli_connect($servername, $username, $password, $database);
 
 if (!$conn) {
@@ -92,50 +86,53 @@ $result = mysqli_query($conn, $query);
                     <td><img src="/pictures/<?php echo $row['cover']; ?>" alt="Cover" width="100"></td>
                     <td>
                         <?php
-                        $username = $_SESSION['username'];
-                        $sql = "SELECT userID FROM users WHERE username = ?";
-                    
-                        if ($stmt = mysqli_prepare($conn, $sql)) {
-                            // Bind the username parameter to the statement
-                            mysqli_stmt_bind_param($stmt, "s", $username);
-                    
-                            // Execute the statement
-                            mysqli_stmt_execute($stmt);
-                    
-                            // Bind the result variable
-                            mysqli_stmt_bind_result($stmt, $userID);
-                    
-                            // Fetch the userID
-                            mysqli_stmt_fetch($stmt);
-                    
-                            // Close the statement
-                            mysqli_stmt_close($stmt);
-                    
-                        $bookID = $row['bookID'];
-
-                        $checkBookToReadQuery = "SELECT * FROM bookstoread WHERE userID='$userID' AND bookID='$bookID'";
-                        $checkReadBooksQuery = "SELECT * FROM readbooks WHERE userID='$userID' AND bookID='$bookID'";
-
-                        $bookToReadResult = mysqli_query($conn, $checkBookToReadQuery);
-                        $readBooksResult = mysqli_query($conn, $checkReadBooksQuery);
-
-                        if (mysqli_num_rows($readBooksResult) > 0) {
-                            echo "Completed";
-                        } elseif (mysqli_num_rows($bookToReadResult) > 0) {
-                            ?>
-                            <form action="moveToFinished.php" method="post">
-                                <input type="hidden" name="book_id" value="<?php echo $row['bookID']; ?>">
-                                <button type="submit">Finished!</button>
-                            </form>
-                            <?php
-                        } else {
-                            ?>
-                            <form action="addToRead.php" method="post">
-                                <input type="hidden" name="book_id" value="<?php echo $row['bookID']; ?>">
-                                <button type="submit">Add to "Books to Read"</button>
-                            </form>
-                            <?php
-                        }}
+                        if (isset($_SESSION['username'])) {
+                            $username = $_SESSION['username'];
+                            $sql = "SELECT userID FROM users WHERE username = ?";
+                        
+                            if ($stmt = mysqli_prepare($conn, $sql)) {
+                                // Bind the username parameter to the statement
+                                mysqli_stmt_bind_param($stmt, "s", $username);
+                        
+                                // Execute the statement
+                                mysqli_stmt_execute($stmt);
+                        
+                                // Bind the result variable
+                                mysqli_stmt_bind_result($stmt, $userID);
+                        
+                                // Fetch the userID
+                                mysqli_stmt_fetch($stmt);
+                        
+                                // Close the statement
+                                mysqli_stmt_close($stmt);
+                        
+                                $bookID = $row['bookID'];
+                        
+                                $checkBookToReadQuery = "SELECT * FROM bookstoread WHERE userID='$userID' AND bookID='$bookID'";
+                                $checkReadBooksQuery = "SELECT * FROM readbooks WHERE userID='$userID' AND bookID='$bookID'";
+                        
+                                $bookToReadResult = mysqli_query($conn, $checkBookToReadQuery);
+                                $readBooksResult = mysqli_query($conn, $checkReadBooksQuery);
+                        
+                                if (mysqli_num_rows($readBooksResult) > 0) {
+                                    echo "Completed";
+                                } elseif (mysqli_num_rows($bookToReadResult) > 0) {
+                                    ?>
+                                    <form action="moveToFinished.php" method="post">
+                                        <input type="hidden" name="book_id" value="<?php echo $row['bookID']; ?>">
+                                        <button type="submit">Finished!</button>
+                                    </form>
+                                    <?php
+                                } else {
+                                    ?>
+                                    <form action="addToRead_action.php" method="post">
+                                        <input type="hidden" name="book_id" value="<?php echo $row['bookID']; ?>">
+                                        <button type="submit">Add to "Books to Read"</button>
+                                    </form>
+                                    <?php
+                                }
+                            }
+                        }
                         ?>
                     </td>
                 </tr>
