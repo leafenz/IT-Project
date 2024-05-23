9e1c9903bda3172ae5b41fc1d96d21c5ac083932
Wiktoria Niziolek <wiktoria.niziolek@gmail.com>
2024-05-23 13:02:30 +0200

    changes to userprofile and update with pictures

--- userProfile.php
+++ userProfile.php
@@ -28,6 +28,7 @@ $result = mysqli_stmt_get_result($stmt);
 if (mysqli_num_rows($result) > 0) {
     // Fetch user data
     $userData = mysqli_fetch_assoc($result);
+    $userID = $userData['userID']; // Get the userID
     ?>
     <!DOCTYPE html>
     <html lang="en">
@@ -47,13 +48,47 @@ if (mysqli_num_rows($result) > 0) {
             <li>Birthdate: <?php echo $userData['birthdate']; ?></li>
             <li>Sex: <?php echo $userData['sex']; ?></li>
         </ul>
-        <a href="logout.php">Logout</a> <!-- Add a logout link -->
+        <a href="logout.php">Logout</a>
 
         <div class="container">
             <div class="books-list">
                 <h2>Books to Read</h2>
-                <!-- List of books to read goes here -->
                 <a href="bookGallery.php" class="add-book-button">Add New Book</a>
+                <ul>
+                <?php
+                // Query to get books to read for the user
+                $sqlBooksToRead = "SELECT b.title, b.cover, b.author FROM bookstoread br
+                                   JOIN books b ON br.bookID = b.bookID
+                                   WHERE br.userID = ?";
+                
+                // Prepare the SQL statement
+                $stmtBooksToRead = mysqli_prepare($conn, $sqlBooksToRead);
+
+                // Bind parameters
+                mysqli_stmt_bind_param($stmtBooksToRead, "i", $userID);
+
+                // Execute the statement
+                mysqli_stmt_execute($stmtBooksToRead);
+
+                // Get the result
+                $resultBooksToRead = mysqli_stmt_get_result($stmtBooksToRead);
+
+                // Check if there are books to read
+                if (mysqli_num_rows($resultBooksToRead) > 0) {
+                    // Fetch and display each book
+                    while ($book = mysqli_fetch_assoc($resultBooksToRead)) {
+                        ?>
+                        <li>
+                            <img src="<?php echo $row['cover']; ?>" alt="Cover" width="100">
+                            <strong><?php echo $book['title']; ?></strong> by <?php echo $book['author']; ?>
+                        </li>
+                        <?php
+                    }
+                } else {
+                    echo "<li>No books to read found.</li>";
+                }
+                ?>
+                </ul>
             </div>
 
             <div class="books-list">
