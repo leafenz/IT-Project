2a2befb5c8f54b14ac77144d9dd98187e1e28e5e
Vee227 <157512338+Vee227@users.noreply.github.com>
2024-06-27 02:00:04 +0200

    Quizzes_Update

--- quizzes.php
+++ quizzes.php
@@ -4,6 +4,10 @@
 <body>
     <iframe width="552" height="167" frameborder="0" src="https://itch.io/embed/2797731"><a href="https://vee22.itch.io/generalquiz">General Quiz</a></iframe>
     <iframe width="552" height="167" frameborder="0" src="https://itch.io/embed/2797801"><a href="https://vee22.itch.io/percyjackson">Percy Jackson</a></iframe>
+    <iframe height="167" frameborder="0" src="https://itch.io/embed/2797954" width="552"><a href="https://vee22.itch.io/harry-potter">Harry Potter</a></iframe>
+    <iframe height="167" frameborder="0" src="https://itch.io/embed/2798003" width="552"><a href="https://vee22.itch.io/the-housemaid-teil-1">The Housemaid (Teil 1)</a></iframe>
+    <iframe width="552" height="167" frameborder="0" src="https://itch.io/embed/2797980"><a href="https://vee22.itch.io/h">The Housemaid&#039;s Secret (Teil 2)</a></iframe>
+    <iframe width="552" height="167" frameborder="0" src="https://itch.io/embed/2798017"><a href="https://vee22.itch.io/nur-noch-ein-einziges-mal">Nur noch ein einziges Mal</a></iframe>
 </body>
 <?php
     include "footer.php"
